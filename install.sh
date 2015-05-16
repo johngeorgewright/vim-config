10669649aa08629cc576592c10e72cb3158debd3
John Wright <johngeorge.wright@gmail.com>
2015-05-16 19:41:34 +0100

    Update install script to include guifonts.

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ echo "\" call project#rc(\"~/Workspace\")" >> $HOME/.vimrc
 echo "\" See https://github.com/amiorin/vim-project#configure on configuring projects" >> $HOME/.vimrc
 
 echo "runtime .gvimrc" > $HOME/.gvimrc
-echo "set guifont=Sauce\\ Code\\ Powerline\\ Plus\\ Nerd\\ File\\ Types:h13" >> $HOME/.gvimrc
+echo "\" set guifont=Sauce\\ Code\\ Powerline\\ Plus\\ Nerd\\ File\\ Types:h13" >> $HOME/.gvimrc
+echo "\" set guifont=Ubuntu\\ Mono\\ derivative\\ Powerline\\ 10" >> $HOME/.gvimrc
 echo "let g:airline_powerline_fonts=1" >> $HOME/.gvimrc
 
